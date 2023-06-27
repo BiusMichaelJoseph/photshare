a88451a3f50ccf05fb2a656897642b424d2b0b4a
BiusMichaelJoseph <biusmichael16@gmail.com>
2023-06-27 15:28:40 +0300

    database photo

--- photos/models.py
+++ photos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=True)
 
     def _str_(self):
         return self.name
