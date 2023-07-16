c1da0cfffd3058d021b019c3ad016107e98acf28
BiusMichaelJoseph <biusmichael16@gmail.com>
2023-07-16 18:01:04 +0300

    2

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+
 def gallery(request):
     return render(request, 'photos/gallery.html')
+
 
 def addPhoto(request):
     return render(request, 'photos/add.html')
