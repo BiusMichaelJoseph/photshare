0cd3f8cc99258e17d18cf1b5f91f33534ae1645c
BiusMichaelJoseph <biusmichael16@gmail.com>
2023-07-20 08:00:31 +0300

    5

--- photos/views.py
+++ photos/views.py
@@ -3,7 +3,7 @@ from .models import Category, Photo
 
 def gallery(request):
     categories = Category.objects.all()
-    photos = Photos.object.all()
+    photos = Photo.objects.all()
     context = { "categories" : categories,"photos":photos }
     return render(request, 'photos/gallery.html', context)
 
