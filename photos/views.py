96e69146479a7400661c5b6a14f41682f9c9656a
BiusMichaelJoseph <biusmichael16@gmail.com>
2023-08-02 22:36:54 +0300

    s

--- photos/views.py
+++ photos/views.py
@@ -8,10 +8,11 @@ def gallery(request):
     return render(request, 'photos/gallery.html', context)
 
 
-def addPhoto(request, pk):
-    Photo = Photo.objects.get(id=pk)
+def addPhoto(request):
     context = { 'Photo':Photo }
-    return render(request, 'photos/add.html', context)
+    return render(request, 'photos/add.html',)
 
 def viewPhoto(request, pk):
+    context = { 'photo':Photo }
+    Photo = Photo.objects.get(id=pk)
     return render(request, 'photos/photo.html')
