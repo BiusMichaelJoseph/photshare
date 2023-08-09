5a9282a6b36b4c463859c11b2484f42fb8a65a90
BiusMichaelJoseph <biusmichael16@gmail.com>
2023-08-09 05:12:23 +0300

    a

--- photos/views.py
+++ photos/views.py
@@ -4,15 +4,16 @@ from .models import Category, Photo
 def gallery(request):
     categories = Category.objects.all()
     photos = Photo.objects.all()
+
     context = { "categories" : categories,"photos":photos }
     return render(request, 'photos/gallery.html', context)
 
+def viewPhoto(request, pk):
+    Photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo':Photo})
 
 def addPhoto(request):
-    context = { 'Photo':Photo }
-    return render(request, 'photos/add.html',)
+    categories = Category.objects.all()
 
-def viewPhoto(request, pk):
-    context = { 'photo':Photo }
-    Photo = Photo.objects.get(id=pk)
-    return render(request, 'photos/photo.html')
+    context = { "categories" : categories,}
+    return render(request, 'photos/add.html', context)
